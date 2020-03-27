a9626ba4e599a584d17aa4e92cbfae0b5dded09e
We-Gold <we.goldm@gmail.com>
2020-03-27 14:20:24 -0400

    Initial commit

---
+++ exercises/Basic frozen lake model/frozen_lake_demo.py
@@ -0,0 +1,33 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+
+env = gym.make('FrozenLake-v0')
+
+# Left = 0 Down = 1 Right = 2 Up = 3
+
+# SFFF
+# FHFH
+# FFFH
+# HFFG
+
+actions = [2,2,1,0,1,0,1,0,2,2,1,0,0,2,2,2]
+
+n_games = 1000
+win_pct = []
+scores = []
+for i in range(n_games):
+    done = False
+    obs = env.reset()
+    score = 0
+    while not done:
+        action = actions[obs]
+        obs, reward, done, info = env.step(action)
+        score += reward
+    scores.append(score)
+
+    if i % 10 == 0:
+        average = np.mean(scores[-10:])
+        win_pct.append(average)
+plt.plot(win_pct)
+plt.show()
